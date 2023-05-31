1949657cf3a06dd284cbcc83cc0687b47d1c67fc
GuruBharathi10 <135030091+GuruBharathi10@users.noreply.github.com>
2023-05-31 18:03:04 +0530

    Add files via upload

---
+++ cbit/rel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ printf(“\n Condition : return values \n”);
+ printf(“\n 10!=10 : %d”,10!=10);
+ printf(“\n 10==10 : %d”,10==10);
+ printf(“\n 10>=10 : %d”,10>=10);
+ printf(“\n 10<=100 : %d”,10<=100);
+ printf(“\n 10!=9 : %d”,10!=9);
+ getch();
+}
